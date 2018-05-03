a1edb9daa3d9ddfc6362427ca91ccc47b2e9b3cc
Halbout Michael <michaelhalbout@gmail.com>
2018-05-03 15:01:47 +0200

    Update arrosageAuto.js

--- desktop/js/arrosageAuto.js
+++ desktop/js/arrosageAuto.js
@@ -344,10 +344,16 @@ function addCmdToTable(_cmd) {
 	tr.append($('<td>')
 		.append($('<input type="hidden" class="cmdAttr form-control input-sm" data-l1key="id">'))
 		.append($('<input class="cmdAttr form-control input-sm" data-l1key="name" value="' + init(_cmd.name) + '" placeholder="{{Name}}" title="Name">')));
+	tr.append($('<td>')
+		  .append($('<div>')
+			.append($('<input class="tooltips cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="minValue" placeholder="{{Min}}" title="{{Min}}" >'))
+			.append($('<input class="tooltips cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="maxValue" placeholder="{{Max}}" title="{{Max}}" >')))
+		  .append($('<div>')
+			.append($('<span class="type" type="' + init(_cmd.type) + '">')
+				.append(jeedom.cmd.availableType()))
+			.append($('<span class="subType" subType="'+init(_cmd.subType)+'">'))));
+	
 	var parmetre=$('<td>');
-	parmetre.append($('<span class="type" type="' + init(_cmd.type) + '">')
-			.append(jeedom.cmd.availableType()));
-	parmetre.append($('<span class="subType" subType="'+init(_cmd.subType)+'">'));
 	if (is_numeric(_cmd.id)) {
 		parmetre.append($('<a class="btn btn-default btn-xs cmdAction" data-action="test">')
 			.append($('<i class="fa fa-rss">')
@@ -371,9 +377,6 @@ function addCmdToTable(_cmd) {
 				.append($('<sup>')
 					.append($('<i class="fa fa-question-circle tooltips" style="font-size : 1em;color:grey;">')
 					.attr('title','Souhaitez-vous afficher cette commande sur le dashboard ?'))))));
-	parmetre.append($('<div>')
-		.append($('<input class="tooltips cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="minValue" placeholder="{{Min}}" title="{{Min}}" >'))
-		.append($('<input class="tooltips cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="maxValue" placeholder="{{Max}}" title="{{Max}}" >')));
 	tr.append(parmetre);
 	$('#table_cmd tbody').append(tr);
 	$('#table_cmd tbody tr:last').setValues(_cmd, '.cmdAttr');
