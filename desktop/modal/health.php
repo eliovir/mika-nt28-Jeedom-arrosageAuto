b52692961f5a6d72425832c2bdcacdf0d907eeeb
Halbout Michael <michaelhalbout@gmail.com>
2018-05-07 07:39:06 +0200

    Create health.php

---
+++ desktop/modal/health.php
@@ -0,0 +1,33 @@
+?php
+if (!isConnect('admin')) {
+	throw new Exception('401 Unauthorized');
+}
+$eqLogics = ChauffeEau::byType('arrosageAuto ');
+?>
+
+<table class="table table-condensed tablesorter" id="table_healthArrosageAuto ">
+	<thead>
+		<tr>
+			<th>{{ID}}</th>
+			<th>{{Nom}}</th>
+			<th>{{Statut}}</th>
+			<th>{{Dernière communication}}</th>
+			<th>{{Date création}}</th>
+		</tr>
+	</thead>
+	<tbody>
+	 <?php
+foreach ($eqLogics as $eqLogic) {
+	echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getId() . '</span></td>';
+	echo '<td><span class="label label-info" style="font-size : 1em; cursor : default;">' . $eqLogic->getName() . '</span></td>';
+	$status = '<span class="label label-success" style="font-size : 1em;cursor:default;">{{OK}}</span>';
+	if ($eqLogic->getStatus('state') == 'nok') {
+		$status = '<span class="label label-danger" style="font-size : 1em;cursor:default;">{{NOK}}</span>';
+	}
+	echo '<td>' . $status . '</td>';
+	echo '<td><span class="label label-info" style="font-size : 1em;cursor:default;">' . $eqLogic->getStatus('lastCommunication') . '</span></td>';
+	echo '<td><span class="label label-info" style="font-size : 1em;cursor:default;">' . $eqLogic->getConfiguration('createtime') . '</span></td></tr>';
+}
+?>
+	</tbody>
+</table>
