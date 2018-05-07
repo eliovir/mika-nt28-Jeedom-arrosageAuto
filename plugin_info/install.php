5c3b2890da2214d50f273f4cdebe9cb745e85e67
Halbout Michael <michaelhalbout@gmail.com>
2018-05-07 07:05:45 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -5,6 +5,9 @@ function arrosageAuto_install(){
 function arrosageAuto_update(){
 	log::add('arrosageAuto','debug','Lancement du script de mise a jours'); 
 	foreach(eqLogic::byType('arrosageAuto') as $arrosageAuto){
+		$cron = cron::byClassAndFunction('arrosageAuto', 'pull',array('Zone_id' => $arrosageAuto->getId()));
+		if (is_object($cron))
+			$cron->remove();
 		$arrosageAuto->save();
 	}
 	log::add('arrosageAuto','debug','Fin du script de mise a jours');
