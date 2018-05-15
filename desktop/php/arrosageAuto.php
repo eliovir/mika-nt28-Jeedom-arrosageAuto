22e8f7c92832e19f68f10d6d3844041d02619d4f
Halbout Michael <michaelhalbout@gmail.com>
2018-05-15 12:06:36 +0200

    Update arrosageAuto.php

--- desktop/php/arrosageAuto.php
+++ desktop/php/arrosageAuto.php
@@ -38,7 +38,7 @@ $eqLogics = eqLogic::byType('arrosageAuto');
 				<center>
 			      		<i class="fa fa-check" style="font-size : 5em;color:#767676;"></i>
 			    	</center>
-			    	<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>Configuration</center></span>
+			    	<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#767676"><center>Testeur d'expression</center></span>
 			</div>
 			<div class="cursor" id="bt_healthArrosageAuto" style="background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
 				<center>
