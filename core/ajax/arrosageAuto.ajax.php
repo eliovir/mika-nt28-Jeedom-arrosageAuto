23ed09c89d26dea9026f68c32f76b40af1e2e66f
Halbout Michael <michaelhalbout@gmail.com>
2018-05-18 13:49:30 +0200

    Update arrosageAuto.ajax.php

--- core/ajax/arrosageAuto.ajax.php
+++ core/ajax/arrosageAuto.ajax.php
@@ -15,7 +15,6 @@ try {
 			exec('sudo mkdir -p '.$uploaddir);
 			exec('sudo chmod 777 -R '.$uploaddir);
 			$uploadfile = $uploaddir . basename($_FILES['PlanImg']['name']);
-			echo '<pre>';
 			if (move_uploaded_file($_FILES['PlanImg']['tmp_name'], $uploadfile))
 				ajax::success(basename($_FILES['PlanImg']['name']);
 			else
