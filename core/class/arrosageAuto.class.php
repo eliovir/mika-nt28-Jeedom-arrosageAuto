c7d6ef491d4e7c2f47067df404092a5a8faa26eb
Halbout Michael <michaelhalbout@gmail.com>
2018-07-17 09:14:38 +0200

    Update arrosageAuto.class.php

--- core/class/arrosageAuto.class.php
+++ core/class/arrosageAuto.class.php
@@ -95,7 +95,7 @@ class arrosageAuto extends eqLogic {
 		$nextTime=null;
 		foreach(config::byKey('Programmations', 'arrosageAuto') as $ConigSchedule){
 			if(array_search($this->getId(),$ConigSchedule["evaluation"]) === false){
-				log::add('arrosageAuto','info',$this->getHumanName().' : La programmation existe deja');
+				log::add('arrosageAuto','debug',$this->getHumanName().' : Zone inactive dans cette programmation : '.json_encode($ConigSchedule)));
 				continue;
 			}
 			$offset=0;
