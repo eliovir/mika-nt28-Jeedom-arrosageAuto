c6a32af69320dfb6add3224eaf04ef576cf16add
Halbout Michael <michaelhalbout@gmail.com>
2018-10-12 11:33:55 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -4,28 +4,12 @@ function arrosageAuto_install(){
 }
 function arrosageAuto_update(){
 	log::add('arrosageAuto','debug','Lancement du script de mise a jours'); 
-	$ProgramationCenter=config::byKey('Programmations', 'arrosageAuto');
-	if($ProgramationCenter == '')
-		$ProgramationCenter=array();
 	foreach(eqLogic::byType('arrosageAuto') as $arrosageAuto){
-		foreach($arrosageAuto->getConfiguration('programation') as $programmation){
-			$ProgramationCenter[]=$programmation;
-		}
-		$arrosageAuto->setConfiguration('programation','');
-		$cron = cron::byClassAndFunction('arrosageAuto', 'pull',array('Zone_id' => $arrosageAuto->getId()));
-		if (is_object($cron))
-			$cron->remove();
-		$cron = cron::byClassAndFunction('arrosageAuto', 'pull',array('id' => $arrosageAuto->getId()));
-		if (is_object($cron)) 	
-			$cron->remove();
-		$arrosageAuto->save();
-		
 		//Mise a jour des statistique
 		$cache = cache::byKey('arrosageAuto::Statistique::'.$arrosageAuto->getId()));
 		if(is_object($cache))
 			$cache->remove()
 	}
-	config::save('Programmations', $ProgramationCenter,'arrosageAuto');
 	log::add('arrosageAuto','debug','Fin du script de mise a jours');
 }
 function arrosageAuto_remove(){
