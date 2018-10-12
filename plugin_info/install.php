4f7c6672d8d51ed0e0c9b5cff59e3de83c5a286b
Halbout Michael <michaelhalbout@gmail.com>
2018-10-12 11:35:01 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -8,7 +8,7 @@ function arrosageAuto_update(){
 		//Mise a jour des statistique
 		$cache = cache::byKey('arrosageAuto::Statistique::'.$arrosageAuto->getId()));
 		if(is_object($cache))
-			$cache->remove()
+			$cache->remove();
 	}
 	log::add('arrosageAuto','debug','Fin du script de mise a jours');
 }
